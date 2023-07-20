7946273b4597e30023464c0f0b7dfc1c5609f82d
Camp Student <campstudent@SG4-3202-03.USG.LOCAL>
2023-07-20 14:26:38 -0400

    USE BLE CODE THAT WORKS

--- DroneHandCode.ino
+++ DroneHandCode.ino
@@ -1,74 +1,106 @@
+// https://learn.sparkfun.com/tutorials/sending-sensor-data-via-bluetooth
 #include <ESP32Servo.h>
 #include <ESP32Tone.h>
 #include <ESP32PWM.h>
-#include <WiFi.h>
-#include <DNSServer.h>
+#include <BLEDevice.h>
 #include "task.h"
 
 /////////////////////////////
 // COPY THESE TO GLOVE CODE AFTER CHANGING
-const char* name = "24f7ad15-fdde-4c83-8327-400a87de818c";
-const char* pass = "9e267cbc-7e40-4d95-be9a-c8f75ba197fa";
+#define DEVICE_NAME         "24f7ad15-fdde-4c83-8327-400a87de818c"
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define THUMB_CHAR_UUID     "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 /////////////////////////////
 
-WiFiServer server(80);
-DNSServer dnsServer;
-IPAddress local_IP(192, 168, 1, 156);
-IPAddress gateway(192, 168, 1, 1);
-IPAddress subnet(255, 255, 0, 0);
-IPAddress primaryDNS(8, 8, 8, 8);
-
+static BLEUUID serviceUUID(SERVICE_UUID);
+static BLEUUID charUUID(THUMB_CHAR_UUID);
+static BLERemoteCharacteristic* thumbCharacteristic;
+static BLEAdvertisedDevice* glove;
 
 Servo thumb;
 
-String getAll(WiFiClient* client) {
-    String all = "";
-    char ch;
-    while (client->available()) all += client->read();
-    return all;
+static void thumbNotify(BLERemoteCharacteristic* characteristic, uint8_t* data, size_t len, bool notify) {
+    Serial.printf("thumbNotify(): %s sent %zu bytes\n", characteristic->getUUID().toString().c_str(), len);
+    int pos = *(int*)data;
+    Serial.printf("Servo pos is %i\n", pos);
+    thumb.write(pos);
+    yield();
 }
 
-Task serverTask("server", 0, NULL, [](void* arg) -> void {
-    WiFiClient client = server.available();
-    if (!client) return;
-    Serial.print("Got an HTTP client at ");
-    Serial.println(client.remoteIP());
-    String all = getAll(&client);
-    if (all.indexOf("HTTP") != -1) {
-        client.println("HTTP/1.1 500 PLEASE DON'T\r\n");
-        client.stop();
-        return;
+bool oktoconnect = false;
+bool connected = false;
+class freezeOnDisconnect : public BLEClientCallbacks {
+    void onConnect(BLEClient* _) {
+        Serial.println("Connected callback!!");
     }
-    int thumbPos = -1;
-    int read = sscanf(all.c_str(), "thumb=%i", &thumbPos);
-    if (read == 1) {
-        client.printf("thumb at %i\n", thumbPos);
-        thumb.write(thumbPos);        
-    } else {
-        client.println("sscanf error");
+    void onDisconnect(BLEClient* _) {
+        //freeze();
+        Serial.println("Disconnected!!");
+        connected = false;
     }
-    client.stop();
-});
+};
 
-Task dnsTask("dns", 0, NULL, [](void* arg) -> void {
-      dnsServer.processNextRequest();
-});
+bool connect() {
+    Serial.println("connecting...");
+    BLEClient* client = BLEDevice::createClient();
+    client->setClientCallbacks(new freezeOnDisconnect());
+    client->connect(glove);
+    client->setMTU(517); // maximum
+    BLERemoteService* service = client->getService(serviceUUID);
+    if (service == NULL) {
+        Serial.println("error getting service");
+        client->disconnect();
+        return false;
+    }
+    thumbCharacteristic = service->getCharacteristic(charUUID);
+    if (thumbCharacteristic == NULL) {
+        Serial.println("error getting thumbCharacteristic");
+        client->disconnect();
+        return false;
+    }
+    if (!thumbCharacteristic->canNotify()) {
+        Serial.println("whoops it isn't a notify");
+        client->disconnect();
+        return false;
+    }
+    thumbCharacteristic->registerForNotify(thumbNotify);
+    Serial.println("Connetcting complete!");
+    return true;
+}
+
+class chooseWithService : public BLEAdvertisedDeviceCallbacks {
+    void onResult(BLEAdvertisedDevice device) {
+        Serial.printf("got advertise: %s\n", device.toString().c_str());
+        if (device.haveServiceUUID() && device.isAdvertisingService(serviceUUID)) {
+            BLEDevice::getScan()->stop();
+            glove = new BLEAdvertisedDevice(device);
+            oktoconnect = true;
+        }
+    }
+};
 
 void setup() {
     Serial.begin(115200);
-    WiFi.softAP(name, pass);
-    Serial.println("Wait for server setup...");
-    delay(100);
-    Serial.println("Wifi setup OK");
-    server.begin();
-    Serial.println("Web server started");
-    dnsServer.start(53, "*", primaryDNS);
-    Serial.println("DNS capture started");
-
+    Serial.println("scanning for glove...");
+    BLEDevice::init("");
+    BLEScan* scan = BLEDevice::getScan();
+    scan->setAdvertisedDeviceCallbacks(new chooseWithService());
+    scan->setInterval(1349);
+    scan->setWindow(449);
+    scan->setActiveScan(true);
+    scan->start(5, false);
     thumb.attach(25);
 }
 
 void loop() {
-    serverTask.run();
-    dnsTask.run();
+    if (oktoconnect && !connected) {
+        connected = connect();
+        if (connected) {
+            Serial.println("connected OK!!");
+        }
+    }
+    if (!connected) {
+        BLEDevice::getScan()->start(0);
+    }
+    yield();
 }
